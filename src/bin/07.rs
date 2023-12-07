3825ff92d7d3aa536ee5acdfefbbb61dcfe36a96
Haymish Francis <58282672+TheArchailect@users.noreply.github.com>
2023-12-07 15:52:59 +0100

    clean up 07

--- src/bin/07.rs
+++ src/bin/07.rs
@@ -8,32 +8,39 @@ enum HandRank {
     ThreeOfAKind,
     FullHouse,
     FourOfAKind,
-    FiveOfAKind,    
+    FiveOfAKind,
 }
 
-fn card_value(card: char) -> u8 {
+fn card_value(card: char, card_values: [u8; 13]) -> u8 {
     match card {
-        'A' => 14,
-        'K' => 13,
-        'Q' => 12,
-        'J' => 11,
-        'T' => 10,
-        _ => card.to_digit(10).unwrap() as u8,
+        'A' => card_values[0],
+        'K' => card_values[1],
+        'Q' => card_values[2],
+        'J' => card_values[3],
+        'T' => card_values[4],
+        '9' => card_values[5],
+        '8' => card_values[6],
+        '7' => card_values[7],
+        '6' => card_values[8],
+        '5' => card_values[9],
+        '4' => card_values[10],
+        '3' => card_values[11],
+        '2' => card_values[12],
+        _ => 0,
     }
 }
 
-fn evaluate_hand(hand: &str) -> (HandRank, [u8; 5]) {
+fn wrangle_hand(hand: &str, card_values: [u8; 13]) -> (HandRank, [u8; 5]) {
     let mut counts = HashMap::new();
-    let card_values_vec: Vec<u8> = hand.chars().map(card_value).collect();
+    let card_values_vec: Vec<u8> = hand.chars().map(|c| card_value(c, card_values)).collect();
 
     // Convert Vec<u8> to [u8; 5]
-    let card_values = match card_values_vec.try_into() {
+    let card_tuple = match card_values_vec.try_into() {
         Ok(arr) => arr,
         Err(_) => panic!("Hand does not have 5 cards"),
     };
-
     for card in hand.chars() {
-        let value = card_value(card);
+        let value = card_value(card, card_values);
         *counts.entry(value).or_insert(0) += 1;
     }
 
@@ -53,170 +60,114 @@ fn evaluate_hand(hand: &str) -> (HandRank, [u8; 5]) {
     }
 
     match (five_count, four_count, three_count, pair_count) {
-        (1, _, _, _) => (HandRank::FiveOfAKind, card_values),
-        (_, 1, _, _) => (HandRank::FourOfAKind, card_values),
-        (_, _, 1, 1) => (HandRank::FullHouse, card_values), 
-        (_, _, 1, _) => (HandRank::ThreeOfAKind, card_values),
-        (_, _, _, 2) => (HandRank::TwoPair, card_values),
-        (_, _, _, 1) => (HandRank::OnePair, card_values),
-        _ => (HandRank::HighCard, card_values),
+        (1, _, _, _) => (HandRank::FiveOfAKind, card_tuple),
+        (_, 1, _, _) => (HandRank::FourOfAKind, card_tuple),
+        (_, _, 1, 1) => (HandRank::FullHouse, card_tuple),
+        (_, _, 1, _) => (HandRank::ThreeOfAKind, card_tuple),
+        (_, _, _, 2) => (HandRank::TwoPair, card_tuple),
+        (_, _, _, 1) => (HandRank::OnePair, card_tuple),
+        _ => (HandRank::HighCard, card_tuple),
     }
 }
 
+fn log_game(game: Vec<(HandRank, [u8; 5], u32)>) {
+    for (index, hand) in game.iter().enumerate() {
+        let rank = &hand.0;
+        let color = hand_rank_color(&rank);
+        let winnings = (index as u32 + 1) * hand.2;
+        println!(
+            "{}: {}{:?} - Hand: {:?} - Bet: {:?} - Winnings: {}{} ",
+            index, color, rank, hand.1, hand.2, winnings, "\x1b[0m"
+        );
+    }
+}
+
+// for console debugging with colour coding
 fn hand_rank_color(rank: &HandRank) -> &str {
     match rank {
-        HandRank::FiveOfAKind => "\x1b[35m", // Magenta
-        HandRank::FourOfAKind => "\x1b[34m", // Blue
-        HandRank::FullHouse => "\x1b[32m", // Green
+        HandRank::FiveOfAKind => "\x1b[35m",  // Magenta
+        HandRank::FourOfAKind => "\x1b[34m",  // Blue
+        HandRank::FullHouse => "\x1b[32m",    // Green
         HandRank::ThreeOfAKind => "\x1b[36m", // Cyan
-        HandRank::TwoPair => "\x1b[33m", // Yellow
-        HandRank::OnePair => "\x1b[31m", // Red
-        HandRank::HighCard => "\x1b[37m", // White
+        HandRank::TwoPair => "\x1b[33m",      // Yellow
+        HandRank::OnePair => "\x1b[31m",      // Red
+        HandRank::HighCard => "\x1b[37m",     // White
     }
 }
 
-fn sort_hands(hands: Vec<(String, u32)>) -> Vec<(HandRank, [u8; 5], u32)> {
-    let mut tupil_hands: Vec<(HandRank, [u8; 5], u32)> = Vec::new();
-    for hand in hands {
-        let hand_rank_values = evaluate_hand(&hand.0);
-        tupil_hands.push((hand_rank_values.0, hand_rank_values.1, hand.1));
+fn process_game_input(
+    game: &str,
+    card_values: [u8; 13],
+    use_wildcard: bool,
+) -> Vec<(HandRank, [u8; 5], u32)> {
+    let mut hands = Vec::new();
+    for line in game.lines() {
+        let parts: Vec<&str> = line.split_whitespace().collect();
+        if parts.len() == 2 {
+            if let Ok(bet) = parts[1].parse::<u32>() {
+                hands.push((parts[0].to_string(), bet));
+            }
+        }
     }
-    tupil_hands.sort();
-    tupil_hands
+
+    let mut tuple_hands: Vec<(HandRank, [u8; 5], u32)> = Vec::new();
+
+    for hand in hands {
+        let (original_hand_rank, original_hand_cards) = wrangle_hand(&hand.0, card_values);
+        let mut best_rank = original_hand_rank;
+
+        if use_wildcard {
+            let card_symbols = [
+                "A", "K", "Q", "T", "9", "8", "7", "6", "5", "4", "3", "2", "J",
+            ];
+
+            let has_wildcard = hand.0.contains('J');
+
+            if has_wildcard {
+                for wildcard_value in card_symbols {
+                    let modified_hand = hand.0.replace('J', &wildcard_value);
+                    let (rank, _) = wrangle_hand(&modified_hand, card_values);
+
+                    if rank > best_rank {
+                        best_rank = rank;
+                    }
+                }
+            }
+        }
+
+        // store the rank / card tuple (as the originals regardless of wildcard replacements) / bet value
+        tuple_hands.push((best_rank, original_hand_cards, hand.1));
+    }
+
+    tuple_hands
 }
 
 pub fn part_one(input: &str) -> Option<u32> {
-    let mut hands = Vec::new();
-
-    for line in input.lines() {
-        let parts: Vec<&str> = line.split_whitespace().collect();
-        if parts.len() == 2 {
-            if let Ok(bet) = parts[1].parse::<u32>() {
-                hands.push((parts[0].to_string(), bet));
-            }
-        }
-    }
-
-    let sorted_hands = sort_hands(hands);
+    const CARD_VALUES: [u8; 13] = [14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2];
+    let mut games_hands = process_game_input(input, CARD_VALUES, false);
+    games_hands.sort();
     let mut sum: u32 = 0;
 
-    for (index, hand) in sorted_hands.iter().enumerate() {
-        let rank = &hand.0;
-        let color = hand_rank_color(&rank);
+    for (index, hand) in games_hands.iter().enumerate() {
         let winnings = (index as u32 + 1) * hand.2;
-        println!("{}: {}{:?} - Hand: {:?} - Bet: {:?} - Winnings: {}{} ",index, color, rank, hand.1, hand.2, winnings, "\x1b[0m");
         sum += winnings;
     }
-
+    // log_game(games_hands);
     Some(sum)
 }
 
-fn part_two_card_value(card: char) -> u8 {
-    match card {
-        'A' => 14,
-        'K' => 13,
-        'Q' => 12,
-        // joker wild card is now the weakest card
-        'J' => 1,
-        'T' => 10,
-        _ => card.to_digit(10).unwrap() as u8,
-    }
-}
-
-fn part_two_pre_evaluate_hand(hand: &str) -> (HandRank, [u8; 5]) {
-    let (original_rank, original_hand_values) = part_two_evaluate_hand(hand);
-    let has_wildcard = hand.contains('J');
-    let mut best_rank = HandRank::HighCard;
-    let card_values = ["A", "K", "Q", "T", "9", "8", "7", "6", "5", "4", "3", "2", "J"];
-
-    if has_wildcard {
-        for wildcard_value in card_values {
-            let modified_hand = hand.replace('J', &wildcard_value);
-            let (rank, _) = part_two_evaluate_hand(&modified_hand);
-            if rank > best_rank {
-                best_rank = rank;
-            }
-        }
-    } else {
-        best_rank = original_rank;
-    }
-    (best_rank, original_hand_values)
-}
-
-fn part_two_evaluate_hand(hand: &str) -> (HandRank, [u8; 5]) {
-    let mut counts = HashMap::new();
-    let card_values_vec: Vec<u8> = hand.chars().map(part_two_card_value).collect();
-
-    // Convert Vec<u8> to [u8; 5]
-    let card_values = match card_values_vec.try_into() {
-        Ok(arr) => arr,
-        Err(_) => panic!("Hand does not have 5 cards"),
-    };
-
-    for card in hand.chars() {
-        let value = part_two_card_value(card);
-        *counts.entry(value).or_insert(0) += 1;
-    }
-
-    let mut pair_count = 0;
-    let mut three_count = 0;
-    let mut four_count = 0;
-    let mut five_count = 0;
-
-    for &count in counts.values() {
-        match count {
-            2 => pair_count += 1,
-            3 => three_count += 1,
-            4 => four_count += 1,
-            5 => five_count += 1,
-            _ => {}
-        }
-    }
-
-    match (five_count, four_count, three_count, pair_count) {
-        (1, _, _, _) => (HandRank::FiveOfAKind, card_values),
-        (_, 1, _, _) => (HandRank::FourOfAKind, card_values),
-        (_, _, 1, 1) => (HandRank::FullHouse, card_values), 
-        (_, _, 1, _) => (HandRank::ThreeOfAKind, card_values),
-        (_, _, _, 2) => (HandRank::TwoPair, card_values),
-        (_, _, _, 1) => (HandRank::OnePair, card_values),
-        _ => (HandRank::HighCard, card_values),
-    }
-}
-
-fn part_two_sort_hands(hands: Vec<(String, u32)>) -> Vec<(HandRank, [u8; 5], u32)> {
-    let mut tupil_hands: Vec<(HandRank, [u8; 5], u32)> = Vec::new();
-    for hand in hands {
-        let hand_rank_values = part_two_pre_evaluate_hand(&hand.0);
-        tupil_hands.push((hand_rank_values.0, hand_rank_values.1, hand.1));
-    }
-    tupil_hands.sort();
-    tupil_hands
-}
-
 pub fn part_two(input: &str) -> Option<u32> {
-    let mut hands = Vec::new();
-
-    for line in input.lines() {
-        let parts: Vec<&str> = line.split_whitespace().collect();
-        if parts.len() == 2 {
-            if let Ok(bet) = parts[1].parse::<u32>() {
-                hands.push((parts[0].to_string(), bet));
-            }
-        }
-    }
-
-    let sorted_hands = part_two_sort_hands(hands);
+    const CARD_VALUES: [u8; 13] = [14, 13, 12, 1, 10, 9, 8, 7, 6, 5, 4, 3, 2];
+    let mut games_hands = process_game_input(input, CARD_VALUES, true);
+    games_hands.sort();
     let mut sum: u32 = 0;
 
-    for (index, hand) in sorted_hands.iter().enumerate() {
-        let rank = &hand.0;
-        let color = hand_rank_color(&rank);
+    for (index, hand) in games_hands.iter().enumerate() {
         let winnings = (index as u32 + 1) * hand.2;
-        println!("{}: {}{:?} - Hand: {:?} - Bet: {:?} - Winnings: {}{} ",index, color, rank, hand.1, hand.2, winnings, "\x1b[0m");
         sum += winnings;
     }
 
+    // log_game(games_hands);
     Some(sum)
 }
 
